e6c61915747faba13e1874662065c795ca21b2c9
whynotfu <73879238+whynotfu@users.noreply.github.com>
2024-12-08 16:47:34 +0300

    Update bubble.cpp

--- sortinMethod/bubble.cpp
+++ sortinMethod/bubble.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-using namespace std;
 
 int main(){
     int N = 5; 
-    int A[N] = {5, 4,3,2,1};
+    int A[N] = {5, 4,3,2,1}; //may be an error cazed by non constant value for an array 
     bool is_sorted = false;
 
     while(not is_sorted){
         int i = 0;
-        is_sorted = true;
+        is_sorted = true; //flag
 
         while(i< N-1){
-        if(A[i]>A[i+1]){
-            int tmp =A[i];
-            A[i] = A[i+1];
-            A[i+1] =tmp;
-            is_sorted = false;
+            if(A[i]>A[i+1]){
+                int tmp =A[i];
+                A[i] = A[i+1];
+                A[i+1] =tmp;
+                is_sorted = false; 
+            }
+            i+=1;
         }
-        i+=1;
-        }
-
     }
+    //O(n2)
     for(int i = 0; i<N; i++){
-        cout<< A[i]<< '\t';
-
+        std::cout<< A[i]<< '\t';
     }
-
-
-    
 }
