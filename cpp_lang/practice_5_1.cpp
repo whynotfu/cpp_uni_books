1104a289c4dc85de63eea2ce9a9005e9067e7ffc
whynotfu <anadtasyadu.0.o@gmail.com>
2024-12-20 01:34:01 +0300

    try2

--- cpp_lang/practice_5_1.cpp
+++ cpp_lang/practice_5_1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 
-
+void start(void);
 /*Написать класс матриц. 
 
 Свойства класса: число строк, число столбцов, одномерный динамический массив для хранения элементов.
@@ -19,12 +19,12 @@ int get_rows() - количество строк
 double get_elem(int i, int j) - вернуть (i,j) элемент 
 */
 
-class matr
-{
+class matr{
 private: 
     int rows;
     int cols; 
     double* data; 
+    
 public:
 //конструктор 
     matr(int rows, int cols){
@@ -51,14 +51,50 @@ public:
     int get_cols() const {
         return cols; 
     }
-    
-    
+    //получение элементов i и j
+    double get_element(int i, int j) const{
+        if(i < 0 || i>= rows || j< 0 || j>cols){
+            std::cout<< "INdex error \n";
+            return 0;
+        }
+        return data[i*cols+j];     
+    } 
+
+    //ввод
+    void input(){
+        std::cout<< "Enter the matrix elements ("<< rows << "X"<<cols<<")\n";
+        for(int i= 0; i< rows; i++){
+            for(int j = 0; j<cols; j++ ){
+                std::cin>> data[i*cols+j];
+            }
+        }  
+    }
+    //вывод 
+    void print() const {
+        for(int i =0; i < rows; i++){
+            for(int j = 0; j<cols; j++){
+                std::cout<< data[i*cols + j ]<< " ";
+            }
+            std::cout<<'\n';
+        }
+    }
+    matr* sum(const matr* mat2) const{ // =сложение матриц
+        if(rows!=mat2 ->get_rows() || cols != mat2 -> get_cols()){
+            std::cout<<"Error matrix are not equil. Can not sum it \n";
+            return nullptr;
+        }
+        matr* result = new matr(rows, cols); 
+        for(int i = 0; i< rows*cols; i++){
+            result -> data[i] + mat2 -> data[i];
+        } 
+        return result;
+    }  
+
 };
 
 
 
 int main(){
-
-
-
+    start();
+    return 0; 
 }
